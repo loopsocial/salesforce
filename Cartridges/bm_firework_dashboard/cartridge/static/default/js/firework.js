b67c52ebb4fe873266e1c307dd8440c3cc97c2ca
Ruturaj Kalal <ruturajkalal@FW-Mac0392.local>
2022-12-14 10:57:17 -0500

    wip

--- Cartridges/bm_firework_dashboard/cartridge/static/default/js/firework.js
+++
@@ -1,7 +0,0 @@
-function resetDashboard(redirectUrl)
-{
-    if (confirm("Are you sure you want to reset Firework Settings ?"))
-    {
-        window.location.href=redirectUrl;
-    }
-}
--- Cartridges/bm_firework_dashboard/cartridge/static/default/js/fireworkfrm.js
+++
@@ -1,5 +0,0 @@
-function onKeyUpFunction() {
-    var str = document.getElementById('fworganizationid').value; //"f_ecom_zyei_001";
-    var split_str = str.split('_').slice(-2)[0] + '_' + str.split('_').slice(-1)[0];
-    document.getElementById('tenantId').value = split_str;
-}
